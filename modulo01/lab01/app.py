adb758c216a0740069fa8fbaa406e188e3dc9da5
Vegatwin02 <vegatwins2210@gmail.com>
2024-02-06 14:23:18 -0400

    update files

---
+++ modulo01/lab01/app.py
@@ -0,0 +1,8 @@
+print('Hola Mundo!')
+mensaje = 'John Doe' # String Value
+gpa = 3.14 # number (real) value
+isValid = True # boolean Value
+print('mensaje')
+print(gpa)
+print(isValid)
+print(f'{mensaje}tu GPA es {gpa:5.2}')
