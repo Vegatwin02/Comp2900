f6266ec1113c9ad9885dafb4aa537415e4919ebf
Vegatwin02 <vegatwins2210@gmail.com>
2024-02-27 13:06:50 -0400

    update my files

--- modulo01/lab02/app.py
+++ modulo01/lab02/app.py
@@ -3,5 +3,5 @@ gpa = float(input('Entre su GPA: '))
 edad = int(input('Ingrese su edad: '))
 
 print(f'Hola {mensaje}!!!, tu GPA es {gpa}')
-print(f'Tu edad es {edad} anos')
+print(f'Tu edad es {edad}')
 
