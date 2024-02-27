1061d96d976823ef9771e3574b4ec66eccd82b3f
Vegatwin02 <vegatwins2210@gmail.com>
2024-02-27 13:29:27 -0400

    update my files

--- modulo02/funcion02/app.py
+++ modulo02/funcion02/app.py
@@ -1,6 +1,14 @@
-def suma(a, b):
+def suma(a=0, b=0):
     return (a + b)
 
 resultado = suma(10, 5)
+
+print(resultado)
+
+resultado = suma()
+
+print(resultado)
+
+resultado = suma(11)
 
 print(resultado)
